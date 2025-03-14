847049ccadcc64fd6667bcca88ef05359a6186e1
MeanHoang <viethoangxtpro08@gmail.com>
2025-03-14 21:33:35 +0700

    CRUD admin

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, HttpStatus, Param, Put, Delete, Patch} from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { Admin } from '../../entities/admin.entity';
 import { ResponseDto } from '../../dtos/response.dto';
@@ -8,11 +8,143 @@ export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
   @Get()
-  async findAll(): Promise<ResponseDto<Admin[]>> {
-    const admins = await this.adminService.findAll();
-    return new ResponseDto<Admin[]>(
+  async findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 5,
+    @Query('search') search?: string,
+  ): Promise<ResponseDto<any>> {
+    console.log(">>> Call API GET list admin!");
+
+    const result = await this.adminService.findAll(page, limit, search);
+
+    return new ResponseDto(
       'success',
-       'Admin list retrieved successfully',
-        admins);
+      'Tải danh sách người dùng thành công',
+      result
+    );
   }
+
+  @Post('/create')
+  async create(@Body() admin: Admin) {
+    try {
+      const newAdmin = await this.adminService.create(admin);
+
+      if (!newAdmin) {
+        return new ResponseDto(
+          'error',
+          'Tên tài khoản đã được sử dụng!',
+          null
+        );
+      }
+
+      return new ResponseDto(
+        'success',
+        'Tạo tài khoản thành công',
+        newAdmin
+      );
+    } catch (error) {
+      return new ResponseDto('error', 'Lỗi tạo tài khoản!', null);
+    }
+  }
+
+  @Put(':id')
+  async update(
+    @Body() adminData:Partial<Admin>,
+    @Param('id') id: number, 
+  ){
+    try {
+      const updatedAdmin = await this.adminService.update(id, adminData);
+
+      if (updatedAdmin === null) {
+        return new ResponseDto(
+          'error',
+          'Không tìm thấy admin cần cập nhật!',
+          null
+        );
+      }
+
+      if (updatedAdmin === 'duplicate') {
+        return new ResponseDto(
+          'error',
+          'Tên đăng nhập đã tồn tại!',
+          null
+        );
+      }
+
+      return new ResponseDto(
+        'success',
+        'Cập nhật admin thành công!',
+        updatedAdmin
+      );
+    } catch (error) {
+      return new ResponseDto('error', 'Lỗi cập nhật tài khoản!', null);
+    }
+  }
+
+  @Delete(':id')
+  async delete(
+    @Param('id') id: number, 
+  ){
+    try {
+      const isDeleted = await this.adminService.delete(id);
+      console.log(">>> check isDeleted: ", isDeleted);
+      if(!isDeleted)     
+        return new ResponseDto(
+        'error',
+        'Không tìm thấy tài khoản',
+         null
+      ); 
+
+      return new ResponseDto(
+        'success',
+        'Xóa tài khoản thành công',
+         id
+      ); 
+    } catch (error) {
+      return new ResponseDto(
+        'error',
+        'Không tìm thấy tài khoản',
+         null
+      ); 
+    }
+  }
+
+  @Get(':id')
+  async getAdminByID(
+    @Param('id') id: number, 
+  ){
+    try {
+      const admin = await this.adminService.getAdminByID(id);
+      if(admin === null)     
+        return new ResponseDto(
+        'error',
+        'Không tìm thấy tài khoản',
+         null
+      ); 
+
+      return new ResponseDto(
+        'success',
+        'Lấy thông tài khoản thành công',
+         admin
+      ); 
+    } catch (error) {
+      return new ResponseDto(
+        'error',
+        'Không tìm thấy tài khoản',
+         null
+      ); 
+    }
+  }
+
+  @Patch('/reset-password/:id')
+  async resetPassword(@Param('id') id: number): Promise<ResponseDto<any>> {
+    const admin = await this.adminService.resetPassword(id);
+
+    if (admin === null) {
+      return new ResponseDto('error', 'Admin không tồn tại!', null);
+    }
+
+    return new ResponseDto('success', 'Reset mật khẩu thành công!', admin);
+  }
+  
 }
--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, Like, FindOptionsWhere, Not  } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import * as dotenv from 'dotenv';
+
 import { Admin } from '../../entities/admin.entity';
 
 @Injectable()
@@ -10,7 +13,124 @@ export class AdminService {
     private readonly adminRepository: Repository<Admin>,
   ) {}
 
-  async findAll(): Promise<Admin[]> {
-    return this.adminRepository.find();
+  //find all
+  async findAll(page: number, limit: number, search?: string) {
+    const whereCondition = search
+      ? { username: Like(`%${search}%`) } 
+      : {}; 
+
+    const [data, total] = await this.adminRepository.findAndCount({
+      where: whereCondition,
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+
+    return {
+      data,
+      total,
+      totalPages: Math.ceil(total / limit),
+      currentPage: page,
+      limit,
+    };
   }
+
+  //create 
+  async create(admin: Admin){
+    //check username 
+    const checkAdmin = await this.adminRepository.findOneBy({ username: admin.username});
+
+    if(checkAdmin){
+      return null;
+    }
+
+    const hashedPassword = await bcrypt.hash(admin.password, 10);
+
+    const finalAdmin = this.adminRepository.create({
+      ...admin,
+      password: hashedPassword,
+    });
+    
+    await this.adminRepository.save(finalAdmin);
+
+      return {
+        id: finalAdmin.id,
+        username: finalAdmin.username,
+        fullname: finalAdmin.fullname,
+        is_active: finalAdmin.is_active,
+        created_at: finalAdmin.created_at,
+        updated_at: finalAdmin.updated_at,
+  };
+  }
+
+  //update
+  async update(id: number, adminData: Partial<Admin>){
+    const admin = await this.adminRepository.findOneBy({ id });
+
+    if (!admin) {
+      return null;
+    }
+
+    // Kiểm tra username có bị trùng với admin khác không
+    if (adminData.username) {
+      const existingAdmin = await this.adminRepository.findOne({
+        where: { username: adminData.username, id: Not(id) },
+      });
+
+      if (existingAdmin) {
+        return 'duplicate'; 
+      }
+    }
+
+    if (adminData.password) {
+      const isSamePassword = await bcrypt.compare(adminData.password, admin.password);
+      if (!isSamePassword) {
+        adminData.password = await bcrypt.hash(adminData.password, 10);
+      } else {
+        delete adminData.password; 
+      }
+    }
+
+    // Cập nhật từng trường nếu được gửi lên
+    Object.assign(admin, adminData);
+
+    return await this.adminRepository.save(admin);
+  }
+
+  //delete 
+  async delete(id: number) {
+    const deleteResult = await this.adminRepository.delete(id);
+    
+    if (deleteResult.affected === 0) {
+      return false;
+    }
+  
+    return true;
+  }
+  
+  async getAdminByID(id: number) {
+    const admin = await this.adminRepository.findOneBy({ id });
+    
+    if (!admin) {
+      return null;
+    }
+  
+    return admin;
+  }
+
+  async resetPassword(id: number): Promise<Admin | null> {
+    const admin = await this.adminRepository.findOneBy({ id });
+
+    if (!admin) {
+      return null;
+    }
+
+    const defaultPassword = process.env.DEFAULT_ADMIN_PASSWORD || '123';
+
+    const newPassword = await bcrypt.hash(defaultPassword, 10);
+    admin.password = newPassword;
+    await this.adminRepository.save(admin);
+
+    return admin;
+  }
+
 }
